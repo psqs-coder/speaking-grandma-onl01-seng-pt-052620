fbfe12709d80ae44e15dcab271bc13c15c92a145
Bruce Parker <detailsharks@gmail.com>
2020-06-06 21:38:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,12 @@
-
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+end
+end
 
 
 
