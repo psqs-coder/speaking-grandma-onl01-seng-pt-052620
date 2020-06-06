628f2c52d7d6493e2c44cc4b677f6837635fc5f4
Bruce Parker <detailsharks@gmail.com>
2020-06-06 21:29:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,12 @@
-
+def speak_to_grandma(phrase)
+         if phrase == "I LOVE YOU GRANDMA!"
+				        "I LOVE YOU TOO PUMPKIN"
+				 elsif phrase != phrase.upcase
+				        "HUH!? SPEAK UP SONNY!"
+				 else phrase == phrase.upcase #if our argument of phrase is EQUAL TO phrase.upcase (uppercase) then its true!
+				        "NO, NOT SINCE 1938!"      #so grandma responds....
+				end
+end 
 
 
 
